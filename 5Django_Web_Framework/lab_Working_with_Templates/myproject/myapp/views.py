16510b2370f6a8dcf46bb57866536b1288e0194f
yanshao113 <139802247+yanshao113@users.noreply.github.com>
2023-08-12 18:08:59 -0700

    Add files via upload

---
+++ 5Django_Web_Framework/lab_Working_with_Templates/myproject/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here for Home, Menu, About and Booking
+
+def home(request):
+    return render(request, "index.html")
+
+def menu(request):
+    return render(request, "menu.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def book(request):
+    return render(request, "book.html")
